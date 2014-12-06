f566b2c2ac12a2d0b0e2831ee1971845c9903449
David Dvorszki <dvorszkid@users.noreply.github.com>
2014-12-06 16:34:06 +0100

    typo fix

--- bashrc.d/90tmux-if-ssh.sh
+++ bashrc.d/90tmux-if-ssh.sh
@@ -2,7 +2,7 @@ if [ -z "$STARTED_TMUX" ] && [ -n "$SSH_TTY" ]
 then
   case $- in
     (*i*)
-      STARTED_STMUX=1; export STARTED_TMUX
+      STARTED_TMUX=1; export STARTED_TMUX
       tmux-home || echo "Tmux failed! Continuing with normal bash startup."
   esac
 fi
