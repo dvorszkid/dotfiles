fe6fe63881e0ae72ba3efeeb1be9e9cbf1f6c2a2
David Dvorszki <dvorszkid@users.noreply.github.com>
2020-04-11 18:00:46 +0200

    apps: added LG WebOS TV control + integration with xrandr control

---
+++ .local/apps/lgwebos/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+##
+# Using https://github.com/supersaiyanmode/PyWebOSTV
+##
+
+from pywebostv.connection import WebOSClient
+from pywebostv.controls import SystemControl, ApplicationControl, SourceControl
+from sys import exit
+from wakeonlan import send_magic_packet
+from argparse import ArgumentParser
+
+exec(open("config.py").read())
+
+if __name__ == "__main__":
+    args = ArgumentParser(description='LG WebOS TV Remote Control')
+    args.add_argument('command', nargs=1, choices=['register', 'test', 'on', 'off'])
+    args = args.parse_args()
+    assert len(args.command) == 1
+    cmd = args.command[0]
+
+    if cmd == 'on':
+        send_magic_packet(tv_mac)
+
+    client = WebOSClient(tv_ip)
+    client.connect()
+
+    def is_me_using():
+        app = ApplicationControl(client)
+        app_id = app.get_current()
+        return app_id == "com.webos.app.hdmi1"
+
+    if cmd == 'register':
+        for status in client.register(store):
+            if status == WebOSClient.PROMPTED:
+                print("Please accept the connect on the TV!")
+            if status == WebOSClient.REGISTERED:
+                print("\nRegistration successful!\nstore = " + str(store))
+                exit(0)
+    else:
+        for status in client.register(store):
+            assert status == WebOSClient.REGISTERED
+
+    if cmd == 'test':
+        c = SystemControl(client)
+        c.notify("test")
+    elif cmd == 'on' and not is_me_using():
+        c = SystemControl(client)
+        source_control = SourceControl(client)
+        sources = source_control.list_sources()
+        hdmi_source = next((s for s in sources if s.label == "HDMI1"), None)
+        if hdmi_source:
+            c.notify("Setting " + str(hdmi_source))
+            source_control.set_source(hdmi_source)
+    elif cmd == 'off' and is_me_using():
+        c = SystemControl(client)
+        c.notify("Bye-bye TV!")
+        c.power_off()
--- .local/bin/xrandr-setup.sh
+++ .local/bin/xrandr-setup.sh
@@ -6,9 +6,11 @@ TV="HDMI-3"
 case "$1" in
 	1)
 		xrandr --output $TV --off
+        lgwebos off
 		;;
 	2)
 		xrandr --output $TV --right-of $MONITOR --mode 1920x1080 --noprimary
+        lgwebos on
 		;;
 	3)
 		xrandr --output $TV --right-of $MONITOR --mode 4096x2160 --noprimary
