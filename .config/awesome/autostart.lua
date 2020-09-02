202cbe904b8d1324fc04526d5f991e4103a492cc
David Dvorszki <dvorszkid@users.noreply.github.com>
2020-09-02 09:38:30 +0200

    awesomewm: in case of VNC, no compton+screensaver

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -5,13 +5,18 @@
 -- common
 local apps = {
     "urxvtd",
-    -- vsync fix, test with: https://www.youtube.com/watch?v=5xkNy9gfKOg
-    "compton -b --backend glx --vsync opengl-swc --paint-on-overlay",
     "unclutter",
-    "xss-lock -n /usr/libexec/xsecurelock/dimmer -l -- xsecurelock",
     "parcellite",
     "tresorit --hidden",
 }
+
+if not os.getenv("VNCDESKTOP") then
+    apps = awful.util.table.join(apps, {
+        -- vsync fix, test with: https://www.youtube.com/watch?v=5xkNy9gfKOg
+        "compton -b --backend glx --vsync opengl-swc --paint-on-overlay",
+        "xss-lock -n /usr/libexec/xsecurelock/dimmer -l -- xsecurelock",
+    })
+end
 
 -- local
 if io.open(".config/awesome/autostartlocal.lua") ~= nil then
