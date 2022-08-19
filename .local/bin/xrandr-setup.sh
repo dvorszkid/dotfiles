7eaee2a332773394ef4baacb2b3332ee0ba75354
David Dvorszki <dvorszkid@users.noreply.github.com>
2022-08-19 08:47:36 +0200

    xrandr-setup.sh: adjust config for dual monitor setup instead of monitor + TV

--- .local/bin/xrandr-setup.sh
+++ .local/bin/xrandr-setup.sh
@@ -1,33 +1,23 @@
 #!/bin/sh
 
-MONITOR="DP-1"
-TV="HDMI-3"
+DELL_2408="DP-1"
+DELL_2415="HDMI-3"
 
 case "$1" in
 	1)
-		xrandr --output $MONITOR --mode 1920x1200 --primary \
-            --output $TV --off
-        lgwebos off &
+		xrandr --output $DELL_2408 --off
 		;;
 	2)
-        lgwebos on &
-		xrandr --output $MONITOR --auto --primary \
-            --output $TV --right-of $MONITOR --mode 1920x1080 --noprimary
+        #lgwebos on &
+		xrandr --output $DELL_2408 --right-of $DELL_2415 --auto --noprimary
 		;;
 	3)
-        lgwebos on &
-		xrandr --output $MONITOR --auto --primary \
-            --output $TV --right-of $MONITOR --mode 4096x2160 --noprimary
+		xrandr --output $DELL_2415 --mode 1920x1200 --primary \
+            --output $DELL_2408 --off
+        #lgwebos off &
 		;;
 	4)
-        lgwebos on &
-		xrandr --output $MONITOR --off \
-            --output $TV --mode 1920x1080 --primary
-		;;
-	5)
-		xrandr --output $TV --off
-		;;
-	6)
-		xrandr --output $TV --right-of "HDMI-2" --auto
+		xrandr --output $DELL_2415 --auto --primary \
+            --output $DELL_2408 --right-of $DELL_2415 --mode 1920x1200 --noprimary
 		;;
 esac
