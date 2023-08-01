eb1decd6e0febfd5973dba86f7359e3b1393264a
David Dvorszki <dvorszkid@users.noreply.github.com>
2023-08-01 10:34:39 +0200

    nvim: key bindings, comments are no longer italic

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -3,13 +3,18 @@ local M = {}
 
 M.disabled = {
   n = {
-    ["<C-c>"] = "",
+    ["<C-c>"] = "", -- Copy file contents to clipboard
+    ["<leader>fw"] = "", -- Telescope live grep
   }
 }
 
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    ["<C-p>"] = {":Telescope find_files <CR>", "Find Files"},
+    ["<leader>fg"] = {":Telescope live_grep <CR>", "Live Grep (rg)"},
+    ["<leader>fr"] = {":Telescope resume <CR>", " Resume last Telescope session"},
   },
 }
 
