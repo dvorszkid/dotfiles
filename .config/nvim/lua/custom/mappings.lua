79ac4373a9b340ca8ea52f2227f8c4d020e3e575
David Dvorszki <dvorszkid@users.noreply.github.com>
2023-08-02 10:26:19 +0200

    nvim: update Telescope mappings

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -5,16 +5,24 @@ M.disabled = {
   n = {
     ["<C-c>"] = "", -- Copy file contents to clipboard
     ["<leader>fw"] = "", -- Telescope live grep
+    ["<leader>fz"] = "", -- Telescope find in current buffer
   }
 }
 
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  }
+}
 
+M.telescope = {
+  n = {
     ["<C-p>"] = {":Telescope find_files <CR>", "Find Files"},
+    ["<C-t>"] = {":Telescope lsp_document_symbols <CR>", "Tags in in current buffer"},
     ["<leader>fg"] = {":Telescope live_grep <CR>", "Live Grep (rg)"},
-    ["<leader>fr"] = {":Telescope resume <CR>", " Resume last Telescope session"},
+    ["<leader>f/"] = {":Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer"},
+    ["<leader>ft"] = {":Telescope lsp_document_symbols <CR>", "Tags in in current buffer"},
+    ["<leader>fr"] = {":Telescope resume <CR>", "Resume last Telescope session"},
   },
 }
 
