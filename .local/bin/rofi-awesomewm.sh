11789256f5028e807ccc50dd997edb49abfe6afd
David Dvorszki <dvorszkid@users.noreply.github.com>
2025-02-08 19:45:33 +0100

    awesomewm: update rofi commands

--- .local/bin/rofi-awesomewm.sh
+++ .local/bin/rofi-awesomewm.sh
@@ -2,8 +2,8 @@
 
 CMDS="xset s activate && sleep 10 && xset dpms force standby    # lock and turn off display
 xset s activate                                           # lock
-loginctl poweroff                                         # shutdown
-loginctl reboot                                           # reboot"
+poweroff                                                  # shutdown
+reboot                                                    # reboot"
 
 CMD=$(echo "$CMDS" | rofi -dmenu -p "AwesomeWM")
 eval $CMD
