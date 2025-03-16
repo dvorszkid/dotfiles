16eafd80bc1307016eb0c4e184adf25af8a92ec1
David Dvorszki <dvorszkid@users.noreply.github.com>
2025-03-16 13:18:43 +0100

    nvim: space+/ searches in current dir

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -13,12 +13,13 @@ return {
     { "<leader><leader>", "<cmd>FzfLua<CR>", desc = "Fzf all sources" },
 
     -- Main
-    { "<C-p>", "<cmd>Fzf files<CR>", desc = "Fzf Files" },
+    { "<C-p>", "<cmd>FzfLua files<CR>", desc = "Fzf Files" },
     {
       "<C-t>",
       "<cmd>FzfLua lsp_document_symbols symbol_width=70 symbol_type_width=10<CR>",
       desc = "Fzf [T]ags in current buffer (LSP)",
     },
+    { "<leader>/", "<cmd>FzfLua live_grep_resume<CR>", desc = "Fzf Grep" },
 
     -- Others
     { "<leader>ft", "<cmd>FzfLua treesitter<CR>", desc = "Fzf [T]ags in current buffer (treesitter)" },
